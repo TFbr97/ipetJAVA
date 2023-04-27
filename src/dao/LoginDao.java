fdf3453473f85dd9efdc42f6a74e38a65327e7d9
404carrot <tiago_silva2008@hotmail.com>
2023-04-27 20:40:48 -0300

    enviando o Login Dao, e o FrLogin
    seus gays!

---
+++ src/dao/LoginDao.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+
+import classe.Funcionario;
+
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author 62127512022.2
+ */
+public class LoginDao {
+    
+      private Connection con;
+      
+    private ResultSet rs;
+    
+    
+    public LoginDao(){
+        
+        this.con = new Conexao().getConnection();
+        
+    }
+    
+    public void buscarLogin(Funcionario funcionario){
+        
+        try{
+            
+            PreparedStatement stmt = con.prepareStatement
+        ("select * from funcionario where usuario='"+funcionario.getUsuario()+"'"+"and senha ='"+funcionario.getSenha()+"'");
+            rs = stmt.executeQuery();
+            if(rs.next()){
+            
+            funcionario.setUsuario(rs.getString("usuario"));
+            funcionario.setSenha(rs.getString("senha"));
+            
+            }
+           // stmt.execute();
+            else{
+            JOptionPane.showMessageDialog(null,"ERRO!"); 
+            //con.close();
+            }
+        }
+        catch(SQLException erro){
+            
+            throw new RuntimeException(erro); 
+        }
+        
+    }
+}
